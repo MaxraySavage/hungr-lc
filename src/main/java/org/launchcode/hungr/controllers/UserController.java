cbaed676c450638175e3beef2262c5991d1571ba
MaxraySavage <maxraysavage@gmail.com>
2021-12-05 18:02:51 -0500

    adds a profile page where you can see other user's recipes

--- src/main/java/org/launchcode/hungr/controllers/UserController.java
+++ src/main/java/org/launchcode/hungr/controllers/UserController.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @ModelAttribute
     public void addUserAttributes(Model model, HttpServletRequest request) {
@@ -24,6 +28,19 @@ public class UserController {
 
     @GetMapping("/profile")
     public String displayProfile(Model model){
+        model.addAttribute("profileOwner", model.getAttribute("user"));
+        return "users/profile";
+    }
+
+    @GetMapping("/profile/{username}")
+    public String displayProfile(Model model, @PathVariable String username){
+        User profileOwner = userRepository.findByUsername(username);
+
+        if(profileOwner == null) {
+            // the expected user was not found
+            return "redirect:/recipes";
+        }
+        model.addAttribute("profileOwner", profileOwner);
 
         return "users/profile";
     }
