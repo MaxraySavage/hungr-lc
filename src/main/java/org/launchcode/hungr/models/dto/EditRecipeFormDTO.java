e77d5bf8c4fa1692c735926bcb425d1ed481c8f7
MaxraySavage <maxraysavage@gmail.com>
2021-12-10 14:52:46 -0500

    use recipe dto and indexing on edit form

---
+++ src/main/java/org/launchcode/hungr/models/dto/EditRecipeFormDTO.java
@@ -0,0 +1,89 @@
+package org.launchcode.hungr.models.dto;
+
+import org.launchcode.hungr.models.Ingredient;
+import org.launchcode.hungr.models.Recipe;
+import org.launchcode.hungr.models.RecipeStep;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EditRecipeFormDTO {
+
+    @NotBlank(message="Name must not be blank.")
+    @Size(max=100, message="Name must be less than 100 characters")
+    private String name;
+
+    @NotBlank(message="Description must not be blank")
+    @Size(max=240, message="Description must be less than 240 characters")
+    private String shortDescription;
+
+    private int id;
+
+    private List<String> ingredients = new ArrayList<>();
+
+    private List<String> steps = new ArrayList<>();
+
+    public EditRecipeFormDTO() {
+        this.steps.add("");
+    }
+
+    public EditRecipeFormDTO(Recipe recipe) {
+        mapFieldsFromRecipe(recipe);
+    }
+
+    public void mapFieldsFromRecipe(Recipe recipe) {
+        this.id = recipe.getId();
+        this.name = recipe.getName();
+        this.shortDescription = recipe.getShortDescription();
+        this.ingredients.clear();
+        for(Ingredient ingredient : recipe.getIngredients()) {
+            this.ingredients.add(ingredient.getName());
+        }
+        this.steps.clear();
+        for(RecipeStep step : recipe.getSteps()) {
+            this.steps.add(step.getText());
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
+}
