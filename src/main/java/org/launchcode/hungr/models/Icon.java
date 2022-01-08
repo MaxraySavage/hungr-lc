ce3bbacfa9f4ce94c225f090f7ed8a60c67ee769
MaxraySavage <maxraysavage@gmail.com>
2022-01-07 23:14:38 -0500

    Implements recipe icons

---
+++ src/main/java/org/launchcode/hungr/models/Icon.java
@@ -0,0 +1,37 @@
+package org.launchcode.hungr.models;
+
+public enum Icon {
+    UTENSILS("fas fa-utensils", "Utensils"),
+    PIZZA("fas fa-pizza-slice", "Pizza"),
+    BACON("fas fa-bacon", "Bacon"),
+    APPLE("fas fa-apple-alt", "Apple"),
+    HAMBURGER("fas fa-hamburger", "Hamburger"),
+    ICECREAM("fas fa-ice-cream", "Ice Cream"),
+    HOTDOG("fas fa-hotdog", "Hot Dog"),
+    FISH("fas fa-fish", "Fish"),
+    DRUMSTICK("fas fa-drumstick-bite", "Drumstick"),
+    CHEESE("fas fa-cheese", "Cheese"),
+    BREAD("fas fa-bread-slice", "Bread"),
+    COOKIE("fass fa-cookie", "Cookie"),
+    HOTPEPPER("fas fa-pepper-hot", "Hot Pepper"),
+    WAFFLE("fas fa-stroopwafel", "Waffle"),
+    LEMON("fas fa-lemon", "Lemon"),
+    CARROT("fas fa-carrot", "Carrot");
+
+    private String fontAwesomeClass;
+
+    private String displayName;
+
+    private Icon(String iconClass, String displayName){
+        this.fontAwesomeClass = iconClass;
+        this.displayName = displayName;
+    }
+
+    public String getFontAwesomeClass() {
+        return fontAwesomeClass;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
